72e7870d55cc0d08a9f36a2d05b8eedc53f622e3
Justin Nice <justinviar200@gmail.com>
2025-11-26 15:12:33 +0700

    Backend

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,3 @@ use Illuminate\Support\Facades\DB;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-Route::get('/test-db', function () {
-    try {
-        $db = DB::connection('mongodb')->getMongoDB();
-        $collections = $db->listCollections();
-
-        return response()->json([
-            'status' => 'OK',
-            'message' => 'MongoDB Connected Successfully!'
-        ]);
-    } catch (\Exception $e) {
-        return response()->json([
-            'status' => 'ERROR',
-            'message' => $e->getMessage()
-        ], 500);
-    }
-});
